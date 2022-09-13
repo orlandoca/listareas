601389b0807c94467770931fcf2a2f2dd31cfbdc
orlandoca <orlandoca647@gmail.com>
2022-09-12 20:44:38 -0400

    cambiando nombre de clase principal, importacion logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo2.png'
 
 function App() {
   return (
-    <div className="App">
-      React
+    <div className="aplicacion-tareas">
+      <div className= 'freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo2' 
+        />
+      </div>
     </div>
   );
 }
